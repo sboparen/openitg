543b63d01bfa6467e8c70e689103ef5d9bf432d7
Simon Parent <sboparen@gmail.com>
2017-03-04 17:41:44 -0500

    Automatically create launcher on desktop.

--- assets/game-data/openitg.sh
+++ assets/game-data/openitg.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 set -e
 cd "$(dirname "$(readlink -f ${BASH_SOURCE[0]})")"
+
+# Create links for data shared between versions.
 if [ ! -e Songs ]; then
     ln -s ../Songs .
 fi
+
+# Create or update launcher on desktop.
+cat >~/Desktop/OpenITG.desktop <<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=$PWD/openitg.sh
+Name=OpenITG
+Icon=$PWD/icon.png
+EOF
+chmod +x ~/Desktop/OpenITG.desktop
+
+# Start OpenITG.
 exec ./openitg "$@"
